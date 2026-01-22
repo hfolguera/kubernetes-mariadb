9146dc7d26fc91c102126313027cc59c77c94b93
Hug Folguera <hfolguera@gmail.com>
2026-01-22 09:21:06 +0100

    fix backup script

--- backup.sh
+++ backup.sh
@@ -13,9 +13,10 @@ NS=mariadb
 POD=`kubectl get pod -n $NS -o jsonpath='{.items[0].metadata.name}'`
 echo "[DEBUG] POD: $POD"
 
-DATABASES=`kubectl exec -i -n $NS $POD -- /usr/bin/mariadb -u root --password=${PASS} -e "SHOW DATABASES;" | grep -v "+-" | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v Warning | awk '{print $1}'`
+DATABASES=`kubectl exec -i -n $NS $POD -- /usr/bin/mariadb -u root --password=${PASS} -e "SHOW DATABASES;"`
 if [ $? -eq 0 ]
 then
+    DATABASES=`echo $DATABASES | tr ' ' '\n' | grep -v "+-" | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v Warning | awk '{print $1}'`
     echo "[DEBUG] DATABASES: $DATABASES"
 else
     echo "[ERROR] Cannot get MariaDB databases!"
@@ -26,7 +27,7 @@ RC=0
 for DB in $DATABASES
 do
     echo "[INFO] Executing backup for $DB"
-    kubectl exec -it -n $NS $POD -- /usr/bin/mysqldump -u root --password=${PASS} $DB > $DEST_PATH/mariadb_${DB}_backup_${DATE}.sql
+    kubectl exec -it -n $NS $POD -- /usr/bin/mariadb-dump -u root --password=${PASS} $DB > $DEST_PATH/mariadb_${DB}_backup_${DATE}.sql
     if [ $? -ne 0 ]
     then
         echo "[ERROR] Cannot execute $DB database backup!"
